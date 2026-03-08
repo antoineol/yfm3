ba4366d18276f4ac7acac1b30bea2ec2c26c2fe2
Antoine Ory-Lamballe <antoine.orylamballe@faktus.eu>
2026-03-08 14:28:10 +0100

    chore default user ID for new

--- src/ui/lib/atoms.ts
+++ src/ui/lib/atoms.ts
@@ -2,6 +2,6 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import type { OptimizeDeckParallelResult } from "../../engine/index-browser.ts";
 
-export const userIdAtom = atomWithStorage("yfm3_user_id", "");
+export const userIdAtom = atomWithStorage("yfm3_user_id", "user_1750627178536_oh9nqp874");
 export const isOptimizingAtom = atom(false);
 export const resultAtom = atom<OptimizeDeckParallelResult | null>(null);
