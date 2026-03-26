778a4c0311440a9180c35b819346e7fcc539adc2
Antoine Ory-Lamballe <antoine.orylamballe@faktus.eu>
2026-03-26 13:21:03 +0100

    add version in downloaded zip

--- docs/onboard/improve-onboarding.md
+++
@@ -1,1 +0,0 @@
-Remastered Perfected download: https://mega.nz/file/SwQwVb5a#1EdeL_Sb8mwvlRodT3sJ3loRjT1kjRfHcvP6eHH3sLo
--- src/ui/features/bridge/bridge-constants.ts
+++ src/ui/features/bridge/bridge-constants.ts
@@ -1,6 +1,5 @@
 import bridgePkg from "../../../../bridge/package.json";
 
 export const BRIDGE_MIN_VERSION: string = bridgePkg.version;
-export const BRIDGE_DOWNLOAD_URL =
-  "https://github.com/antoineol/yfm3/releases/latest/download/yfm-bridge-win-x64.zip";
+export const BRIDGE_DOWNLOAD_URL = `https://github.com/antoineol/yfm3/releases/download/bridge-v${BRIDGE_MIN_VERSION}/yfm-bridge-win-x64-v${BRIDGE_MIN_VERSION}.zip`;
 export const DUCKSTATION_URL = "https://www.duckstation.org";
